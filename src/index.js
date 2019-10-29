00dce5b099b1e9583d98b63dc896480747ad1e80
amandena <manda.panda91@yahoo.com>
2019-10-28 21:42:21 -0400

    set up store in index.js

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ import {Provider} from 'react-redux'
 import App from './App'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, 
+document.getElementById('root'));
