ec27dc5a74fb6b7f8ac011ef5b4cfbeb794ac016
amandena <manda.panda91@yahoo.com>
2019-11-07 21:11:19 -0500

    added handleChange to Box

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
 class Box extends React.Component {
+  state = {
+    number: ''
+  }
+
+  // getValue() {
+  //   const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  //   const {value} = this.props
+  //
+  //   if (values.includes(value)) {
+  //     return this.props.value
+  //   } else if (value === '0') {
+  //     return null
+  //   }
+  // }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
     return (
       <div>
         <form>
-
+          <input type="text" value={this.state.name} name="number" onChange={this.handleChange} />
         </form>
       </div>
     )
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,7 @@ const Grid = props => {
 
   return (
     <div>
-      {props.games.map(game => <li key={game.id}>{game.numbers}, {game.difficulty}, {game.won}</li>)}
+      
     </div>
   )
 }
