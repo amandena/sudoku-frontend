5dbc45f028184f4f96ca7637a422ed27a0973088
amandena <manda.panda91@yahoo.com>
2019-11-11 23:24:55 -0500

    added Redirect to Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 const Game = props => {
 
   let game = props.games[props.match.params.id - 1]
-  
+
   return (
     <div>
+    {game ? null : <Redirect to='/games' />}
     {game ? game.numbers : null}
     {game ? game.difficulty : null}
     {game ? game.won : null}
--- src/components/Games.js
+++ src/components/Games.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import Game from './Game'
+import {Link} from 'react-router-dom'
+
 
 const Games = props => {
 
   return (
     <div>
-      {props.games.map(game => <div key={game.id}><Game game={game}/></div>)}
+      {props.games.map(game => <div key={game.id}>
+        <Link to={`/games/${game.id}`}>{game.difficulty}</Link>
+      </div>)}
     </div>
   )
 }
