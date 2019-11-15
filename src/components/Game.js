726100ddfec929a977f8f312094ed8b9d3fee3e4
amandena <manda.panda91@yahoo.com>
2019-11-14 23:17:59 -0500

    working on styling for Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Row from './Row'
+import '../css/Board.css'
 // import {Redirect} from 'react-router-dom'
 
 const Game = props => {
@@ -12,15 +13,17 @@ const Game = props => {
       {game ? game.numbers : null}
       {game ? game.difficulty : null}
       {game ? game.won : null}
-      <Row id="1"/>
-      <Row id="2"/>
-      <Row id="3"/>
-      <Row id="4"/>
-      <Row id="5"/>
-      <Row id="6"/>
-      <Row id="7"/>
-      <Row id="8"/>
-      <Row id="9"/>
+      <div className="board">
+        <Row id="1"/>
+        <Row id="2"/>
+        <Row id="3"/>
+        <Row id="4"/>
+        <Row id="5"/>
+        <Row id="6"/>
+        <Row id="7"/>
+        <Row id="8"/>
+        <Row id="9"/>
+      </div>
     </div>
   )
 }
