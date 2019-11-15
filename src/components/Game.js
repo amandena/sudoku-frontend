87450cbcb6956a758569bda30a29fc18c8cec01e
amandena <manda.panda91@yahoo.com>
2019-11-14 22:50:21 -0500

    added Row components to Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,7 +12,15 @@ const Game = props => {
       {game ? game.numbers : null}
       {game ? game.difficulty : null}
       {game ? game.won : null}
-      <Row />
+      <Row id="1"/>
+      <Row id="2"/>
+      <Row id="3"/>
+      <Row id="4"/>
+      <Row id="5"/>
+      <Row id="6"/>
+      <Row id="7"/>
+      <Row id="8"/>
+      <Row id="9"/>
     </div>
   )
 }
